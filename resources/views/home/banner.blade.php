839a7bb450f5f32fb2168343e97393a11a402af4
Mohamad Kabalane <mkabalane@gmail.com>
2022-03-04 14:09:55 +0200

    master

--- resources/views/home/banner.blade.php
+++ resources/views/home/banner.blade.php
@@ -1,22 +1,6 @@
 @php $banners = \App\Banner::all();  @endphp
 @if($banners)
-    <!-- Banner Area -->
-{{--    <div class="banner-area" style="background-image: url( {{ 'storage/'.str_replace('\\', '/', $banner->image) }});">
-        <div class="container">
-            <div class="banner-content wow fadeInUp" data-wow-delay="00ms" data-wow-duration="1000ms">
-                <h1 class="wow fadeInLeft" data-wow-delay="100ms" data-wow-duration="1000ms">@if($lang=='en') {{$banner->title_en}} @else {{$banner->title_ar}}  @endif</h1>
-                <p class="wow fadeInLeft" data-wow-delay="200ms" data-wow-duration="1000ms">@if($lang=='en') {{$banner->text_en}} @else {{$banner->text_ar}}  @endif</p>
-                <a href="about.html" class="default-btn  wow fadeInLeft"  data-wow-delay="300ms" data-wow-duration="1200ms">{{ __('translation.learn-more') }}</a>
-                <div class="banner-shape">
-                    <img src="{{ asset('images/shape/shape1.png')  }}" alt="Banner Images">
-                    <img src="{{ asset('images/shape/shape2.png')  }}" alt="Banner Images">
-                    <img src="{{ asset('images/shape/shape3.png')  }}" alt="Banner Images">
-                </div>
-            </div>
-        </div>
-    </div>--}}
-    <!-- Banner Area End -->
-    <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+    {{--<div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
         <div class="carousel-indicators">
             @foreach($banners as $key => $banner)
             <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="{{$key}}" class="@if($key==0) active @endif" aria-current="@if($key==0) true @endif" aria-label="Slide {{ $key + 1  }}"></button>
@@ -41,5 +25,44 @@
             <span class="carousel-control-next-icon" aria-hidden="true"></span>
             <span class="visually-hidden">Next</span>
         </button>
+    </div>--}}
+    <div class="banner-slider-area">
+        <div class="banner-slider owl-carousel owl-theme owl-loaded owl-drag">
+            <div class="owl-stage-outer owl-height" style="height: 521.047px;">
+                <div class="owl-stage"
+                     style="transition: all 0.25s ease 0s; width: 10885px; transform: translate3d(-4665px, 0px, 0px);">
+                    @foreach($banners as $key => $banner)
+                    <div class="owl-item cloned" style="width: 1555px;">
+                        <div class="banner-item">
+                            <div class="container">
+                                <div class="row align-items-center">
+                                    <div class="col-lg-6">
+                                        <div class="banner-item-content">
+                                            <h1>@if($lang=='en') {{$banner->title_en}} @else {{$banner->title_ar}}  @endif</h1>
+                                            <p> @if($lang=='en') {{$banner->text_en}} @else {{$banner->text_ar}}  @endif</p>
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-6 pe-0">
+                                        <div class="banner-item-img">
+                                            <img src=" {{ 'storage/'.str_replace('\\', '/', $banner->image) }}" alt="Banner Images">
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    @endforeach
+                </div>
+            </div>
+            <div class="owl-nav disabled">
+                <button type="button" role="presentation" class="owl-prev"><span aria-label="Previous">‹</span></button>
+                <button type="button" role="presentation" class="owl-next"><span aria-label="Next">›</span></button>
+            </div>
+        </div>
+        <div class="banner-shape-two">
+            <img src="{{ asset('images/shape/shape1.png')  }} " alt="Banner Images">
+            <img src="{{ asset('images/shape/shape2.png')  }} " alt="Banner Images">
+            <img src="{{ asset('images/shape/shape3.png')  }}" alt="Banner Images">
+        </div>
     </div>
 @endif
